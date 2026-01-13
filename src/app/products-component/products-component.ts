cb7d0241bfc88c13675afeb91b9d05242128add8
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-01-13 06:05:04 -0500

    creacion del metodo ngOnInit

--- src/app/products-component/products-component.ts
+++ src/app/products-component/products-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product.model';
 @Component({
@@ -7,12 +7,16 @@ import { Product } from '../models/product.model';
   templateUrl: './products-component.html',
   styleUrl: './products-component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   products: Product[] = [];
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
     this.productService.getProducts().subscribe((products) => {
       this.products = products;
     });
