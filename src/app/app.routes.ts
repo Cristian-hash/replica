f6a5467f9c879f43a8d22c379a954c7d0842004e
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-01-26 05:56:47 -0500

    correcciones de links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './products-component/products-component';
 import { CartComponent } from './cart-component/cart-component';
 export const routes: Routes = [
+  { path: '', redirectTo: 'producto', pathMatch: 'full' },
   {
     path: 'producto',
     component: ProductsComponent,
